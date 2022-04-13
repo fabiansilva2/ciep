89ead5b8c89d1985fcd15317b6fe6502e2a95611
Fabian Silva <fabiansilva2@users.noreply.github.com>
2022-04-12 22:00:41 -0400

    Primeira Modificação
    Primeiro commit

---
+++ js/01-custom-places-example.js
@@ -0,0 +1,26 @@
+function runExample3() {
+    $("#custom-places").mapsed({
+		showOnLoad: 	
+		[
+			
+			{
+				
+				autoShow: true,
+				lat: 53.79,
+				lng:-1.5426760000000286,
+				name: "SmartEDU",
+				street: "Over the rainbow, Up high way",
+				userData: 99
+			}
+
+		]
+		
+	});									
+}
+
+
+$(document).ready(function() {
+	runExample3();
+});
+
+
